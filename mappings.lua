dd9d9d28608bde224568bfeeebf8991b69393151
FadI G <fadi.gourie@noaignite.com>
2024-03-07 11:44:11 +0100

    prev & next buffer mapping

--- mappings.lua
+++ mappings.lua
@@ -11,7 +11,9 @@ return {
     ["<leader>bn"] = { "<cmd>tabnew<cr>", desc = "New tab" },
     ["<leader>bD"] = {
       function()
-        require("astronvim.utils.status").heirline.buffer_picker(function(bufnr) require("astronvim.utils.buffer").close(bufnr) end)
+        require("astronvim.utils.status").heirline.buffer_picker(
+          function(bufnr) require("astronvim.utils.buffer").close(bufnr) end
+        )
       end,
       desc = "Pick to close",
     },
@@ -19,7 +21,10 @@ return {
     -- this is useful for naming menus
     ["<leader>b"] = { name = "Buffers" },
     -- quick save
-    -- ["<C-s>"] = { ":w!<cr>", desc = "Save File" },  -- change description but the same command
+
+    -- navigate buffers
+    ["<S-h>"] = { ":bprevious<CR>", desc = "Navigate to previous buffer" },
+    ["<S-l>"] = { ":bnext<CR>", desc = "Navigate to next buffer" }, -- ["<C-s>"] = { ":w!<cr>", desc = "Save File" },  -- change description but the same command
   },
   v = {
     -- stay in indent mode
